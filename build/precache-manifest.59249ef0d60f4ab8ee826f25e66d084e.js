90d399a9b6c13ca7b4a5a7c6368ef32cf63affc7
Tristan K <tristanmkernan@gmail.com>
2020-04-30 19:33:08 -0400

    release: v0.1

---
+++ build/precache-manifest.59249ef0d60f4ab8ee826f25e66d084e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09f890e68007e89f42a3741e72cfdbfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c1305187e0379d51e2b",
+    "url": "/static/css/2.a2c18f5e.chunk.css"
+  },
+  {
+    "revision": "9bbe175d80808ccf4805",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "1c1305187e0379d51e2b",
+    "url": "/static/js/2.96a5046d.chunk.js"
+  },
+  {
+    "revision": "58c9f2c1d92dbffe76d2a30ff09d0854",
+    "url": "/static/js/2.96a5046d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bbe175d80808ccf4805",
+    "url": "/static/js/main.4df6f764.chunk.js"
+  },
+  {
+    "revision": "23d95ea0216e05674557",
+    "url": "/static/js/runtime-main.fdfd6580.js"
+  }
+]);
